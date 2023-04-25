472a94bb8289b6b30f9bae0fde3aad38312e147a
Fahiran <fffahiran@gmail.com>
2023-04-25 17:33:14 +0530

    initail commit

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 """backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,29 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.conf import settings # new
-from  django.conf.urls.static import static #new
+from django.urls import path
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Backend Online Shop APIS",
-        default_version='1.0.0',
-        description="This is swagger for our apis."
-
-    ),
-    public=True,
-)
-
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('onlineshop.urls')),
-    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
-   
+    path('', index),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for backend project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
